55ebb4182c7f49a6e8a35250f95e1aa2da9a14bb
wuc <429215265@qq.com>
2018-06-01 23:27:17 +0800

    文件下载 HttpUrlConnect

---
+++ app/src/main/java/com/wuc/downloader/download/DownloadListener.kt
@@ -0,0 +1,25 @@
+package com.wuc.downloader.download
+
+import java.io.File
+
+interface DownloadListener {
+
+    /**
+     * 下载成功
+     * @param file 下载的文件
+     */
+    fun onSuccess(file: File)
+
+    /**
+     * 下载中
+     * @param total 总大小(单位:B)
+     * @param download 已下载大小
+     */
+    fun onDownloading(total: Long, download: Long)
+
+    /**
+     * 下载失败
+     */
+    fun onFailure()
+
+}
